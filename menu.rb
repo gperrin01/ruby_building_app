9481205ec45d3c74bcbfadad36ff66f2fec64867
Guillaume Perrin <gperrin01@gmail.com>
2015-05-20 19:09:41 +0100

    building menu: functions for flat

--- menu.rb
+++ menu.rb
@@ -1,16 +1,38 @@
 def menu
   puts `clear`
   puts "*** Hello Owner, what area are we working on today? ***"
-  print "Manage (B)uilding or (F)lats, or (q)uit"
+  print "Manage (B)uilding or (A)partment, or (q)uit"
   choice = gets.chomp.downcase
-  case choce
+  # add stuff for quit
+  case choice
   when 'b'
     menu_building
-  when 'f'
-    menu_flats
+  when 'a'
+    menu_apartments
+  when 'q'
+    return
   end
 end
 
-def menu_flats
-  puts "*** Manage Flats ***"
-  print "(C)reate a flat, (R)egister new tenant, (A)dd tenant to a flat, or (q)uit?: "
+def menu_apartments
+  puts "*** Manage Apartments ***"
+  print "(C)reate an apartment, (R)egister new tenant, (A)dd tenant to a apartment, or back (u)p in the menu or (q)uit?: "
+  case choice
+  when 'c'
+    create_flat
+  when 'r'
+    register_tenant
+  when 'a'
+    add_tenant
+  when 'u'
+    menu
+  when 'q'
+    return
+  end
+end
+
+def create_flat
+  options = {}
+  print "Price: "
+  options[:price] = gets.to_f
+end
